53987d5232e4810184634b9fa1610a1d9e034c52
Josef Strillinger <jstrillinger@tsn.at>
2022-01-30 20:02:16 +0100

    rest

--- Strillinger_ccde_aufgabe_08_JSON_und_REST/API_OpenWeatherMap/open_weather_map.py
+++ Strillinger_ccde_aufgabe_08_JSON_und_REST/API_OpenWeatherMap/open_weather_map.py
@@ -3,10 +3,13 @@ import json
 
 import ids
 
-url = 'http://api.openweathermap.org/data/2.5/weather?q=Innsbruck&appid=%s&units=metric' % ids.openweathermap_key
+url = 'http://api.openweathermap.org/data/2.5/air_pollution?lat=47.2627&lon=11.3945&appid=%s' % ids.openweathermap_key
+url2 = 'http://api.openweathermap.org/data/2.5/weather?q=Innsbruck&appid=%s&units=metric' % ids.openweathermap_key
 
 if __name__ == '__main__':
-    response = requests.get(url).json()
+    responsePollution = requests.get(url).json()
+    responseWeather = requests.get(url2).json()
+    '''
     print(response)
     # Formatierte Ausgabe
     print(json.dumps(response, indent=4, sort_keys=False))
@@ -26,3 +29,36 @@ if __name__ == '__main__':
 
     description = response["weather"][0]["description"]
     print(description)
+    '''
+    '''print(json.dumps(responsePollution, indent=4, sort_keys=False))'''
+    co = str(responsePollution['list'][0]['components']['co'])
+    no2 = str(responsePollution['list'][0]["components"]["no2"])
+    o3 = str(responsePollution['list'][0]["components"]["o3"])
+    pm10 = responsePollution['list'][0]["components"]["pm10"]
+    
+    print("Kohlenstoffmonoxid: ")
+    print(co)
+    print("\nStickstoffdioxid: ")
+    print(no2)
+    print("\nFeinstaub: ")
+    print(pm10)
+    print("\nOzon: ")
+    print(o3)
+    
+    print("--------------------------------------------------------------")
+    '''print(json.dumps(responseWeather, indent=4, sort_keys=False))'''
+    
+    temp = responseWeather['main']['temp']
+    tempFeel = responseWeather['main']['feels_like']
+    humidity = responseWeather['main']['humidity']
+    
+    print("\nTemperatur: ")
+    print(temp)
+    print("\nGefühlt: ")
+    print(tempFeel)
+    print("\nLuftfeuchtigkeit: ")
+    print(humidity)
+    
+    
+    
+    
--- Strillinger_ccde_aufgabe_08_JSON_und_REST/Millionaire/model.py
+++ Strillinger_ccde_aufgabe_08_JSON_und_REST/Millionaire/model.py
@@ -3,11 +3,12 @@ import random
 
 class Question:
     
-    def __init__(self, level, question, answers, correctAnswer):
+    def __init__(self, level, question, answers, correctAnswer, id):
         self.level=level
         self.question=question
         self.answers=answers
         self.correctAnswer=correctAnswer
+        self.id=id
     
     def __str__(self):      
        return str(self.level)+" "+" "+self.question+" | "+str(self.answers)+" | "+str(self.correctAnswer)
@@ -17,12 +18,14 @@ def getData(filename):
     f.readline()
     lines=f.readlines()
     questions=[]
+    i = 1
     for item in lines:
+        i+=1
         question=item.split("\t")
         answers=[question[2],question[3],question[4],question[5]]
         random.shuffle(answers)
         correct=answers.index(question[2])
-        q1= Question(question[0],question[1],answers,correct)
+        q1= Question(question[0],question[1],answers,correct, i)
         questions.append(q1)
     return questions
     
