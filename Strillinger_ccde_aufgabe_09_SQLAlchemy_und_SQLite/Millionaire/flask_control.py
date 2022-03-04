2b6860738b8f9227483941c661a2a98e375ab713
Josef Strillinger <jstrillinger@tsn.at>
2022-03-04 23:50:50 +0100

    db update

--- Strillinger_ccde_aufgabe_09_SQLAlchemy_und_SQLite/Millionaire/flask_control.py
+++ Strillinger_ccde_aufgabe_09_SQLAlchemy_und_SQLite/Millionaire/flask_control.py
@@ -1,7 +1,7 @@
 import json
-from flask_restful import Api
-from model import Question, Millionaire, getRandomQuestion, getData, Service, AllQuests, getQuests
-from flask import Flask, render_template, session
+from flask_restful import Api, Resource
+from model import Question, Millionaire, getRandomQuestion, getData, getQuests, get_question_by_id, change_question, add_question, delete_question, get_all_questions
+from flask import Flask, jsonify, render_template, request, session
 
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ api = Api(app)
 
 @app.route('/')
 def start():
-   session["level"]=0
+   session["difficulty"]=0
    return render_template("startseite.html")
 
 @app.route('/questions')
@@ -25,22 +25,50 @@ def ranQuest():
 @app.route('/game/<int:answer>')
 def game(answer = -1):
    if answer != -1:
-      if answer == (session["correct"]+1):
-         if session["level"] == 4:
+      if answer == (session["correct_answer"]+1):
+         if session["difficulty"] == 4:
             #level auf 0, korrekte Antwort löschen; gewinner seite anzeigen
-            session["level"] = 0
-            session.pop('correct', None)
+            session["difficulty"] = 0
+            session.pop('correct_answer', None)
             return render_template("end.html", result=dict, text="Sie haben gewonnen!")
-         session["level"]+=1
+         session["difficulty"]+=1
       else:
-         session["level"] = 0
+         session["difficulty"] = 0
          return render_template("end.html", result=dict, text="Sie haben verloren!")
 
-   q= getRandomQuestion(session["level"], getQuests())
+   q= getRandomQuestion(session["difficulty"], getQuests())
    
-   session["correct"] = q.correctAnswer
-   return render_template("game.html",result=dict, level = session["level"], question=q)
+   session["correct_answer"] = q.correctAnswer
+   return render_template("game.html",result=dict, level = session["difficulty"], question=q)
 
+class Service(Resource):
+   def get(self, id):
+      return get_question_by_id(id)
+        
+    
+   def put(self, id):
+      if not get_question_by_id:
+         return {"500: Frage mit ID: {} existiert bereits!".format(id)}
+      data = request.get_json(force=True)['info']
+      add_question(id, data)
+      return {"200: Frage mit ID: {} wurde hinzugefügt!".format(id)}
+      
+    
+   def patch(self, id):
+      data=json.loads(request.json['info'])
+      change_question(id, data)
+      return {'message': 'object with id %d modified' % id}
+     
+   def delete(self, id):
+      delete_question(id)
+      return {'message':'%d deleted' % id}
+
+
+#TODO All-Questions
+
+class AllQuests(Resource):
+    def get(self):
+      get_all_questions
 
 
 
--- Strillinger_ccde_aufgabe_09_SQLAlchemy_und_SQLite/Millionaire/model.py
+++ Strillinger_ccde_aufgabe_09_SQLAlchemy_und_SQLite/Millionaire/model.py
@@ -1,38 +1,31 @@
 #from distutils.log import info
+import sqlite3
 from random import randint
 import random
-from flask import Flask, request, jsonify, session
+from flask import Flask, jsonify, render_template, request, redirect, session, g
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.sql.expression import func
-from sqlalchemy import create_engine
 from flask_restful import Resource, Api
 from dataclasses import dataclass
-from sqlalchemy import Column, Integer, Text
+from sqlalchemy import Column, Integer, Text, Float, DateTime, create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.sql.expression import func
 import json
 
 Base = declarative_base()
 metadata = Base.metadata
 
-engine = create_engine('sqlite:///millionaire.sqlite3')
+engine = create_engine('sqlite:///.\millionaire.sqlite3')
 db_session = scoped_session(sessionmaker(autocommit=True, autoflush=True, bind=engine))
-Base.queary = db_session.query_property()
+Base.query = db_session.query_property()
 app = Flask(__name__)
 api = Api(app)
 
 class Millionaire(Base):
     __tablename__ = 'millionaire'
     
-    id: int
-    difficulty: int
-    question: str
-    correct_answer: str
-    answer2: str
-    answer3: str
-    answer4: str
-    background_information: str
-
     id = Column(Integer, primary_key=True)
     difficulty = Column(Integer)
     question = Column(Text)
@@ -41,6 +34,16 @@ class Millionaire(Base):
     answer3 = Column(Text)
     answer4 = Column(Text)
     background_information = Column(Text)
+    
+    def serialize(self):
+        return {
+            "ID": self.id,
+            "level": self.difficulty,
+            "fragetext": self.question,
+            "antwortmoeglichkeit": [self.correct_answer, self.answer2, self.answer3, self.answer4],
+            "antwort": 0
+        }
+    
 
 class Question:
     
@@ -59,7 +62,9 @@ class Question:
     
 
 def getData():
-    infos = Millionaire.queary.all()
+    infos = Millionaire.query.all()
+    #for x in infos:
+    #    print(x.serialize())
     questions=[]
     i = 0
     for info in infos:
@@ -67,7 +72,7 @@ def getData():
         answers=[info.correct_answer,info.answer2,info.answer3,info.answer4]
         random.shuffle(answers)
         #correct=answers.index(infos[id].correct_answer)
-        q1= Question(info.difficulty, info.question, answers,info.correct_answer, info.id)#auf db umschreiben, danach ===> profit
+        q1= Question(info.difficulty, info.question, answers, answers.index(info.correct_answer), info.id)#auf db umschreiben, danach ===> profit
         questions.append(q1)
         i+=1
     return questions
@@ -87,54 +92,48 @@ def getQuests():
 
 #TODO Service Class
 
-class Service(Resource):
-    def get(self, id):
-        info = Millionaire.queary.get(id)
-        return jsonify(info)
-    
-    def put(self, id):
-        data = request.get_json(force=True)['info']
-        print(data)
-        info = Millionaire(difficulty=data['difficulty'], question=data['question'], correct_answer=data['correct_answer'], answer2=data['answer2'], answer3=data['answer3'], answer4=data['answer4'], background_information=['background_information'])
-        db_session.add(info)
-        db_session.flush()
-        return jsonify(info)
-    
-    def patch(self, id):
-        info = Millionaire.queary.get(id)
-        if info is None:
-            return jsonify({'message':'object with id %d does not nexist' % id})
-        data=json.loads(request.json['info'])
-        if 'difficulty' in data:
-            info.difficulty = data['difficulty']
-        if 'question' in data:
-            info.question = data['question']
-        if 'correct_answer' in data:
-            info.correct_answer = data['correct_answer']
-        if 'answer2' in data:
-            info.answer2 = data['answer2']
-        if 'answer3' in data:
-            info.answer3 = data['answer3']
-        if 'answer4' in data:
-            info.answer4 = data['answer4']
-        if 'background_information' in data:
-            info.background_information = data['background_information']
-        db_session.add(info)
-        db_session.flush()
-        return jsonify({'message': 'object with id %d modified' % id})
-    
-    def delete(self, id):
-        info = Millionaire.queary.get(id)
-        if info is None:
-            return jsonify({'message':'object with id %d does not exist' % id})
-        db_session.delete(info)
-        db_session.flush()
-        return jsonify({'message':'%d deleted' % id})
+def get_question_by_id(id):
+    data = Millionaire.query.get(id)
+    if data == None:
+        return {"ERROR"}
+    return data.serialize()
 
+def add_question(id, data):
+    info = Millionaire(difficulty=data['difficulty'], question=data['question'], correct_answer=data['correct_answer'], answer2=data['answer2'], answer3=data['answer3'], answer4=data['answer4'], background_information=['background_information'])
+    db_session.add(info)
+    db_session.flush()
+    return info.serialize()
 
-#TODO All-Questions
+def change_question(id, data):
+    info = Millionaire.query.get(id)
+    if info is None:
+        return jsonify({'message':'object with id %d does not nexist' % id})
+    if 'difficulty' in data:
+        info.difficulty = data['difficulty']
+    if 'question' in data:
+        info.question = data['question']
+    if 'correct_answer' in data:
+        info.correct_answer = data['correct_answer']
+    if 'answer2' in data:
+        info.answer2 = data['answer2']
+    if 'answer3' in data:
+        info.answer3 = data['answer3']
+    if 'answer4' in data:
+        info.answer4 = data['answer4']
+    if 'background_information' in data:
+        info.background_information = data['background_information']
+    db_session.add(info)
+    db_session.flush()
+    return {"200: Frage mit ID: {} wurde geändert!".format(id)}
 
-class AllQuests(Resource):
-    def get(self):
-        infos = Millionaire.queary.all()
-        return jsonify(infos)
+def delete_question(id):
+    info = Millionaire.query.get(id)
+    if info is None:
+        return jsonify({'message':'object with id %d does not exist' % id})
+    db_session.delete(info)
+    db_session.flush()
+    return True
+
+def get_all_questions():
+    infos = Millionaire.query.all()
+    return infos.serialize()
